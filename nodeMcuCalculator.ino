ca871bc9a14c69636fac1f0cf6d4f1c4137d0493
Ismael126895 <71767811+Ismael126895@users.noreply.github.com>
2023-08-22 14:49:36 +0300

    testing MCU and calculator

---
+++ nodeMcuCalculator.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+float firstNum;
+float secNum;
+float answer;
+char operand;
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  Serial.begin(9600);
+}
+
+void loop() {
+  lcd.setCursor(0,0);
+  lcd.print("Input 1st Number");
+  while (Serial.available()==0){
+    
+  }
+  firstNum=Serial.parseFloat();
+  Serial.println(firstNum);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Input 2nd Number");
+  Serial.read();
+  while (Serial.available()==0){
+    
+  }
+  secNum=Serial.parseFloat();
+  Serial.println(firstNum);
+  Serial.println(secNum);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  
+  lcd.print("Input Operator(+,-,*,/)");
+  Serial.read();
+  while (Serial.available()==0){
+    
+  }
+  operand=Serial.read();
+  switch (operand){
+  case '+':
+  answer=firstNum+secNum;
+  break;
+  case '-':
+  answer=firstNum+secNum;
+  break;
+  case '*':
+  answer=firstNum+secNum;
+  break;
+  case '/':
+  answer=firstNum/secNum;
+  break;
+  }
+  
+  
+  Serial.println(answer);
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(firstNum);
+  lcd.print(operand);
+  lcd.print(secNum);
+  lcd.setCursor(0,1);
+  lcd.print(" = ");
+  lcd.print(answer);
+  delay(10000);
+}
